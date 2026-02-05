a6f3acf293fa4a5950a29a58aaf3a2f74ff67da4
Gundabathula Prem Dev Kumar <gpremdevkumar@gmail.com>
2026-02-05 11:28:17 +0530

    Added Topic 01: Java Basics explanation

---
+++ 01_Basics_of_Java_Programming/javaBasicInfo.java
@@ -0,0 +1,8 @@
+class JavaBasicsInfo {
+    public static void main(String[] args) {
+        System.out.println("Java is a high-level, object-oriented language.");
+        System.out.println("JVM executes bytecode");
+        System.out.println("JRE provides runtime environment");
+        System.out.println("JDK is used for development");
+    }
+}
